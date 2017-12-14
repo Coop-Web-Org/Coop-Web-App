b34e879c5ba7098cdc1b377999f880e5dd0d9e21
Jezreel D Tan <34290328+jezzar042015@users.noreply.github.com>
2017-12-14 12:26:35 +0800

    Add files via upload

---
+++ myJs.js
@@ -0,0 +1,16 @@
+
+function loginon() {
+    document.getElementById("login-overlay").style.display = "block";
+}
+
+function loginoff() {
+    document.getElementById("login-overlay").style.display = "none";
+}
+
+function signupon() {
+    document.getElementById("signup-overlay").style.display = "block";
+}
+
+function signupoff() {
+    document.getElementById("signup-overlay").style.display = "none";
+}
